12a14a9da4a522da2851ba9cc198b23dfc2543d3
Wenderson Santos <souowendy@gmail.com>
2023-09-09 23:11:31 -0300

    Initial Commit

---
+++ src/main/java/com/souowendy/aquiexpress/AquiExpressApplication.java
@@ -0,0 +1,13 @@
+package com.souowendy.aquiexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquiExpressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AquiExpressApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/souowendy/aquiexpress/AquiExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souowendy.aquiexpress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquiExpressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
