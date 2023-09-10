53838b9d7d7c2816e4886ba5bbafe901000b8268
Wenderson Santos <souowendy@gmail.com>
2023-09-10 01:30:25 -0300

    Disable Spring Security

--- src/main/java/com/souowendy/aquiexpress/configurations/SecurityConfiguration.java
+++ src/main/java/com/souowendy/aquiexpress/configurations/SecurityConfiguration.java
@@ -1,2 +1,19 @@
-package com.souowendy.aquiexpress.configurations;public class SecurityConfiguration {
+package com.souowendy.aquiexpress.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
 }
